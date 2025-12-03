4eb3880481192b3cd7ef40ab2c5cca2097b24b1f
ManonChab <godfroymanon@gmail.com>
2025-12-03 10:23:11 +0100

    functioning fecth and buttons

--- features.js
+++ features.js
@@ -1,10 +1,38 @@
-fetch("https://uselessfacts.jsph.pl/api/v2/facts/random.json",{
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-    },
-})
-    .then(Response => Response.json())
-    .then(y => document.getElementById("factLocation").innerHTML =y)
-    .finally(console.log("fact ok"));
-    
+const factUrl = "https://uselessfacts.jsph.pl/api/v2/facts/random"
+const todayUrl = "https://uselessfacts.jsph.pl/api/v2/facts/today"
+
+const newFact = document.getElementById("factLocation");
+const dailyFact = document.getElementById("factLocation");
+
+function factFetch() {
+    fetch(factUrl)
+        .then((data) => data.json())
+        .then((fact) => {
+            newFact.textContent = fact.text
+        })
+}
+
+function clickForFact(){
+    const factButton = document.getElementById("newFactButton");
+    factButton.addEventListener("click", factFetch);
+}
+
+clickForFact();
+
+
+function dailyFetch() {
+    fetch(todayUrl)
+        .then((data) => data.json())
+        .then((fact) => {
+            dailyFact.textContent = fact.text
+        })
+}
+
+function clickForTodayFact(){
+    const factButton = document.getElementById("dailyFactButton");
+    factButton.addEventListener("click", dailyFetch);
+}
+
+clickForTodayFact();
+
+
