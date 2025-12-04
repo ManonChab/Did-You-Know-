bc9d47b53e326372f37a9283d2e3fff8b9ee8960
ManonChab <godfroymanon@gmail.com>
2025-12-04 22:48:31 +0100

    some CSS changes

--- features/favorite2.js
+++ features/favorite2.js
@@ -27,24 +27,48 @@ needFavList();
 const arrayFav = [];
 
 function addToFav(fact) {
-    const listFav = document.querySelector(".factLocation");
+
 
     const object = {
         text: factPlace.textContent}
 
     arrayFav.push(object)
 
+    const listItem = document.createElement("li");
+    listItem.textContent = object.text;
+    listItem.classList.add("favFact")
+
     console.log("added:", object);
     console.log(arrayFav);
+    console.log(listItem);
 }
 
 async function clickToFav () {
-    const favButton = document.getElementById("empty-star")
+    const favButton = document.getElementById("full-star")
     let fact = await factFetch();
     const factText = fact.text || fact;
-    favButton.addEventListener("click", () => addToFav(factText))
+    favButton.addEventListener("click", () => addToFav(factPlace.textContent))
 }
 
 clickToFav();
+
+async function toggleFavList() {
+    const body = document.querySelector(".factFrame");
+    const favButton = document.getElementById("button-full-star");
+
+    favButton.addEventListener("click", () => {
+        const currentContent = document.querySelector(".factFrame");
+        const listFav = document.querySelector(".listFav");
+        const listItem = document.querySelector(".favFact")
+
+         if (arrayFav.length >= 1) {
+            currentContent.style.display = "none";
+            listItem.style.display = "block";
+         }
+
+    });
+}
+
+toggleFavList();
 
 export {arrayFav}
